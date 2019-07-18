bcf6d3e4189ac2d3a10138366747324918f4d67f
Aaron Bugher <aabugher@gmail.com>
2019-07-17 21:59:37 -0700

    Still working on what to touch.

--- touchup.sh
+++ touchup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+touch ca/root/conf
 touch ca/root/cert.pem 
 touch ca/root/crl.pem 
 touch ca/root/crl.der
+touch ca/intermediate/conf
+touch ca/intermediate/csr.pem 
 touch ca/intermediate/cert.pem 
 touch ca/intermediate/crl.pem 
 touch ca/intermediate/crl.der
